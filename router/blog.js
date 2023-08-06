9c4e5a445f8fe0c168e82f41b9e1b5f21dd0d4d1
Harmedino <adamsahmed191@gmail.com>
2023-08-06 08:45:31 +0100

    code splicted success

--- router/blog.js
+++ router/blog.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { getUser, sendPost, updateBlog } = require("../controller/blog");
+const {
+  getUser,
+  sendPost,
+  updateBlog,
+  getSingleBlog,
+} = require("../controller/blog");
 
 const router = express.Router();
 
 router.get("/getBlog", getUser);
 router.post("/sendPost", sendPost);
 router.patch("/update/:id", updateBlog);
+router.get("/getBlog", getSingleBlog);
 
 module.exports = router;
