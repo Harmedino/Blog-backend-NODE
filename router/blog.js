881fcb24380f83e8469204adc06d38ae15326434
Harmedino <adamsahmed191@gmail.com>
2023-08-07 11:48:03 +0100

    routing done

--- router/blog.js
+++ router/blog.js
@@ -4,6 +4,7 @@ const {
   sendPost,
   updateBlog,
   getSingleBlog,
+  deleteBlog,
 } = require("../controller/blog");
 
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 router.get("/getBlog", getUser);
 router.post("/sendPost", sendPost);
 router.patch("/update/:id", updateBlog);
-router.get("/getBlog", getSingleBlog);
+router.get("/getBlog/:id", getSingleBlog);
+router.delete("/deleteBlog/:id", deleteBlog);
 
 module.exports = router;
