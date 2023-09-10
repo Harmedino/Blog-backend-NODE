92922587aea0912ef07bfd9807fde7a476fd4738
Harmedino <adamsahmed191@gmail.com>
2023-09-10 17:42:18 +0100

    cookie error fixed

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./router/blog");
 const auth = require("./router/authentication");
-const mail = require('./router/message')
+const mail = require('./router/message');
 const path = require("path");
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// CORS Configuration (adjust for production)
 app.use(
   cors({
     origin: ["http://localhost:3000"],
@@ -19,9 +23,10 @@ app.use(
   })
 );
 
-// app.use(express.static("public"));
+// Serve uploaded files
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// Connect to MongoDB
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
@@ -29,9 +34,6 @@ async function connectDB() {
       useUnifiedTopology: true,
     });
     console.log("Connected to MongoDB");
-    const PORT = 5000;
-    app.listen(PORT, () => {
-    });
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
   }
@@ -39,8 +41,18 @@ async function connectDB() {
 
 connectDB();
 
+// Mount your routes
 app.use("/", userRoute);
-
 app.use("/", auth);
+app.use("/", mail);
 
-app.use('/',mail)
+// Error handling middleware (add your own error handling logic)
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "An error occurred" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
